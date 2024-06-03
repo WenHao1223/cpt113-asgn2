cc7a3d0fddfe7c2fbc9be880ff3c5215ee9e3603
Lim Wen Hao <wenhaojshs@gmail.com>
2024-06-04 01:08:42 +0800

    Move all implementation to header file

--- CardContent.h
+++ CardContent.h
@@ -33,4 +33,53 @@ class CardContent {
     string getDescription() const;
 };
 
+template <class T1, class T2>
+CardContent<T1,T2>::CardContent() {
+  category = "";
+  description = "";
+}
+
+template <class T1, class T2>
+CardContent<T1,T2>::~CardContent() {}
+
+template <class T1, class T2>
+void CardContent<T1,T2>::setLabel(T1 label) {
+  this->label = label;
+}
+
+template <class T1, class T2>
+void CardContent<T1,T2>::setQuestion(T2 question) {
+  this->question = question;
+}
+
+template <class T1, class T2>
+void CardContent<T1,T2>::setCategory(string category) {
+  this->category = category;
+}
+
+template <class T1, class T2>
+void CardContent<T1,T2>::setDescription(string description) {
+  this->description = description;
+}
+
+template <class T1, class T2>
+T1 CardContent<T1,T2>::getLabel() const {
+  return label;
+}
+
+template <class T1, class T2>
+T2 CardContent<T1,T2>::getQuestion() const {
+  return question;
+}
+
+template <class T1, class T2>
+string CardContent<T1,T2>::getCategory() const {
+  return category;
+}
+
+template <class T1, class T2>
+string CardContent<T1,T2>::getDescription() const {
+  return description;
+}
+
 #endif
